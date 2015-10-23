ea48fafbba2868f77b94745da2b4c859a668d79a
Robbert Müller <spam.me@grols.ch>
2015-10-23 21:13:17 +0200

    updated config

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,16 +1,17 @@
 node /^node\d+/ {
-	class{ 'apt':
+  class{ 'apt':
     purge                => {
       'sources.list'     => true,
       'sources.list.d'   => true,
     }
-	}
+  }
 
-	include apt
+  include ::apt
+  include ::apt::update
 
-	apt::source { 'debian':
-		location    => 'http://ftp.nl.debian.org/debian/',
-		repos       => 'main contrib non-free',
+  apt::source { 'debian':
+    location    => 'http://ftp.nl.debian.org/debian/',
+    repos       => 'main contrib non-free',
     key         => {
       id        => 'A1BD8E9D78F7FE5C3E65D8AF8B48AD6246925553',
       server    => 'pool.sks-keyservers.net'
@@ -18,10 +19,10 @@ node /^node\d+/ {
     include     => {
       src       => false
     }
-	}
-	apt::source { 'debian-security':
-		location    => 'http://security.debian.org/',
-		repos       => 'main contrib non-free',
+  }
+  apt::source { 'debian-security':
+    location    => 'http://security.debian.org/',
+    repos       => 'main contrib non-free',
     key         => {
       id        => 'A1BD8E9D78F7FE5C3E65D8AF8B48AD6246925553',
       server    => 'pool.sks-keyservers.net'
@@ -30,11 +31,11 @@ node /^node\d+/ {
       src       => false
     },
     release     => 'wheezy/updates'
-	}
+  }
 
-	apt::source { 'puppetlabs':
-		location   => 'http://apt.puppetlabs.com',
-		repos      => 'main',
+  apt::source { 'puppetlabs':
+    location   => 'http://apt.puppetlabs.com',
+    repos      => 'main',
     key         => {
       id        => '47B320EB4C7C375AA9DAE1A01054B7A24BD6EC30',
       server    => 'pool.sks-keyservers.net'
@@ -42,23 +43,30 @@ node /^node\d+/ {
     include     => {
       src       => false
     }
-	}
+  }
 
-	class { 'corosync':
-		enable_secauth    => true,
-		authkey => 'puppet:///modules/corosync/authkey',
-		bind_address      =>  [ $ipaddress_eth1, $ipaddress_eth2 ],
-		multicast_address => [ '239.1.1.2', '239.1.2.2' ],
-		port => [ 5405, 5415 ],
-		rrp_mode => 'active',
-		require => Class['apt'],
-	}
-	corosync::service { 'pacemaker':
-		version => '0',
-	}
+  Class['apt::update'] -> Package<| |>
 
-	user { "vagrant":
-		ensure => present,
-		shell  => "/bin/bash",
-	}
+  class { 'corosync':
+    enable_secauth    => true,
+    threads           => 1,
+    authkey           => 'io+ÂÉ´kJ6¸ègÜpáb&Ú|õ?Õb.§¹]»ÆJ/ç4]Æ¶üNçFñì¤ËKSô¥éÙêüru©¯ãùÎÙceÝ+Oäu´-_B?pg b×bãFM4²X\ÖºÀFÖ½
+ø
+,ò',
+    authkey_source    => 'string',
+    bind_address      => [ $ipaddress_eth1, $ipaddress_eth2 ],
+    multicast_address => [ '239.1.1.2', '239.1.2.2' ],
+    port              => [ 5405, 5415 ],
+    rrp_mode          => 'active',
+  }
+
+  corosync::service { 'pacemaker':
+    version => '0',
+  }
+
+  user { "vagrant":
+    ensure => present,
+    shell  => "/bin/bash",
+  }
+
 }
